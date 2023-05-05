6c97fd82c33df65fd5db28420acdbee1fff65548
Jungwoo <evencoding@gmail.com>
2023-05-05 18:56:17 +0900

    fix: 모달의 초기 상태가 true 때 #modal-root를 찾지 못하는 버그 수정

--- dist/useModalSwitch.js
+++ dist/useModalSwitch.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 var useModalSwitch = function (initState) {
     var _a = useState(initState || false), isModalOpen = _a[0], setModalState = _a[1];
+    var _b = useState(isModalOpen), openWhenInitRender = _b[0], setOpenWhenInitRender = _b[1];
     useEffect(function () {
-        if (isModalOpen) {
+        if (initState) {
             document.body.appendChild(modalRoot);
         }
     });
@@ -12,6 +13,10 @@ var useModalSwitch = function (initState) {
         return root;
     };
     var modalRoot = useState(getModalRoot())[0];
+    if (openWhenInitRender) {
+        document.body.appendChild(modalRoot);
+        setOpenWhenInitRender(false);
+    }
     var openModal = function () {
         if (!modalRoot || document.body.contains(modalRoot))
             return;
