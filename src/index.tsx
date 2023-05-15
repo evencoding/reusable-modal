9fd35cbb3abd0142663acbfb913ed83d5e354942
Jungwoo <evencoding@gmail.com>
2023-05-15 23:36:04 +0900

    chore: version 2.0.0 배포 준ㅂ;

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ModalProvider } from './context/ModalContext';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <ModalProvider initState={true}>
-      <App />
-    </ModalProvider>
-  </React.StrictMode>
-);
+export { useModal } from './useModal';
